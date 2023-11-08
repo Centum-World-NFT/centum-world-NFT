73a2c8acc3742874e9a4d111372f59a1c49cc8dc
centum-world <centumworldminingrig@gmail.com>
2023-11-08 12:32:15 +0530

    collections table heading

--- client/src/pages/collections/NftCollections.jsx
+++ client/src/pages/collections/NftCollections.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HeadingDiv, HeadingRoute, HeadingTitle, Wrapper } from './NftCollectionsStyle'
+import TableHeading from '../../components/common/tableheading/TableHeading'
 
 const NftCollections = () => {
   return (
@@ -9,6 +10,7 @@ const NftCollections = () => {
           <HeadingTitle variant="h5" >Leaderboard</HeadingTitle>
           <HeadingRoute>View collections</HeadingRoute>
         </HeadingDiv>
+        <TableHeading/>
       </Wrapper>
     </div>
   )
