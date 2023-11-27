fc89ced8359c6165d089ae01d6eae93cd7f1cead
aniimessh <animeshgupta2.002@gmail.com>
2023-11-27 12:09:08 +0530

    colors pallate added for devlopment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="font-Poppins">Hello World</h1>
+      <Header />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      'Poppins': ["Poppins, 'sans-serif'"]
+      Poppins: ["Poppins, 'sans-serif'"],
+    },
+    colors: {
+      vulcan: {
+        50: "#f5f5fa",
+        100: "#ebebf3",
+        200: "#d2d3e5",
+        300: "#abaece",
+        400: "#7e83b2",
+        500: "#5e6499",
+        600: "#4a4d7f",
+        700: "#3d3f67",
+        800: "#353757",
+        900: "#30304a",
+        950: "#14141f",
+      },
     },
   },
   plugins: [],
