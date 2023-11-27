a15d6a45d07f020b2b7444709fb269e443a1a046
aniimessh <animeshgupta2.002@gmail.com>
2023-11-27 14:17:01 +0530

    hero section updated with design

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -4,6 +4,8 @@ const ContentSection = () => {
       <div className="h-24 w-24 slide-top absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600"></div>
       <div className="h-24 w-24 slide-right absolute border rounded-full border-violet-600  right-0"></div>
       <div className="h-16 w-16 slide-right absolute border rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 top-0 right-60"></div>
+      <div className="h-5 w-5 slide-top absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 left-60"></div>
+      <div className="h-20 w-20 slide-top absolute border rounded-full border-indigo-600 bottom-0 left-60"></div>
       <div className="w-full">
         <div>
           <p className="font-Poppins text-4xl text-vulcan-50 font-extrabold text-right mb-5">
