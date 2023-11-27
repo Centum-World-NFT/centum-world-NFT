e402947e57a5fb825abfc725388b3e24323faf64
aniimessh <animeshgupta2.002@gmail.com>
2023-11-27 11:53:19 +0530

    files and folder changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homepage/HomePage";
-import NftCards from "./pages/nftcards/NftCards";
-import PageTitle from "./components/common/pagetitle/PageTitle";
-import NftFilter from "./components/common/nftfilter/NftFilter";
-import NftCollections from "./pages/collections/NftCollections";
-
-
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/discover" element={<PageTitle/>}  >
-          <Route path="nft" element={<NftFilter />} />
-          <Route path="collections" element={<NftCollections/>} />
-        </Route>
-        {/* <Route path="/collection" element={<NftCollections />} /> */}
-      </Routes>
+      <h1>Hello World</h1>
     </>
   );
 }
