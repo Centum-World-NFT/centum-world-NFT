bd25237dc9ef95c68d7da69b32b9f26aa02ed1b9
aniimessh <animeshgupta2.002@gmail.com>
2023-11-28 11:39:27 +0530

    feat: cards finished

--- src/components/AuctionSection.jsx
+++ src/components/AuctionSection.jsx
@@ -4,6 +4,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import cardIMg from "../assets/img/cardItem.jpg";
+import creatorAV from "../assets/img/creatorAV.jpg";
 
 const AuctionSection = () => {
   const breakpoints = {
@@ -43,7 +44,7 @@ const AuctionSection = () => {
             {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
               <>
                 <SwiperSlide key={index}>
-                  <div className="h-96 w-80 bg-vulcan-900 rounded-md p-4 flex-col">
+                  <div className="h-max w-80 bg-vulcan-900 rounded-md p-4 flex-col">
                     <div className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
                       <img src={cardIMg} alt="" className="rounded-md" />
                       <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-violet-600 to-indigo-600 opacity-0 transition duration-300 ease-in-out hover:opacity-70 flex items-center justify-center">
@@ -58,6 +59,31 @@ const AuctionSection = () => {
                     <div className="mt-5 font-Poppins text-vulcan-50 font-bold flex justify-between">
                       <p>&ldquo;Hii This is React Basic..</p>
                       <p className="text-violet-600">Python</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className="flex items-center w-max gap-2">
+                        <img
+                          src={creatorAV}
+                          alt=""
+                          className="h-10 w-10 rounded-lg"
+                        />
+                        <div>
+                          <p className="font-Poppins text-xs opacity-50">
+                            Creator
+                          </p>
+                          <a href="" className="font-Poppins text-md font-bold text-vulcan-50 hover:text-vulcan-500 transition-all duration-200">
+                            Jhon Doe
+                          </a>
+                        </div>
+                      </div>
+                      <div>
+                          <p className="font-Poppins text-xs opacity-50">
+                            Price
+                          </p>
+                          <p className="font-Poppins text-md font-bold text-vulcan-50">
+                            4999/-
+                          </p>
+                        </div>
                     </div>
                   </div>
                 </SwiperSlide>
