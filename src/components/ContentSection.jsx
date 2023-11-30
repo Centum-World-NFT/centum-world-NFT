6f953c8cfca87f1b5f01bcce0f83dcf75bb9a63d
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-11-30 17:49:46 +0530

    button index updated

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -6,7 +6,7 @@ const ContentSection = () => {
       <div className="h-24 w-24 slide-top absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 -z-10"></div>
       <div className="h-24 w-24 slide-right absolute border rounded-full border-violet-600 -z-10 top-14  right-0"></div>
       <div className="h-16 w-16 slide-right absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 top-0 right-60  -z-10"></div>
-      <div className="h-5 w-5 slide-top absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 left-60"></div>
+      <div className="h-5 w-5 slide-top absolute rounded-full bg-gradient-to-r from-violet-600 to-indigo-600 left-60 -z-30"></div>
       <div className="h-20 w-20 slide-top absolute border rounded-full border-indigo-600 bottom-0 left-60"></div>
       <div>
         <p className="font-Poppins text-4xl text-vulcan-50 font-extrabold text-right mb-5 z-20">
